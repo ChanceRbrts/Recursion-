71aaafe53b74950b290a1e217d09d1290cce462c
PlatPlat48 <chanceroberts@engr2-2-64-135-dhcp.int.colorado.edu>
2015-08-28 14:37:42 -0600

    Added Sprite Creator
    Haven’t done anything with it yet, but the sprite creator’s now here!
    …Also… WHY DOES GITHUB NOT SAY THAT I HAVE BEEN EDITING MY OWN PROJECT?!

---
+++ Recursion?/SpriteCreator.h
@@ -0,0 +1,13 @@
+//
+//  SpriteCreator.h
+//  Recursion?
+//
+//  Created by Chance Roberts on 8/28/15.
+//  Copyright (c) 2015 Chance Roberts. All rights reserved.
+//
+
+#ifndef Recursion__SpriteCreator_h
+#define Recursion__SpriteCreator_h
+@interface SpriteCreator : NSObject
+@end
+#endif
---
+++ Recursion?/SpriteCreator.m
@@ -0,0 +1,12 @@
+//
+//  SpriteCreator.m
+//  Recursion?
+//
+//  Created by Chance Roberts on 8/28/15.
+//  Copyright (c) 2015 Chance Roberts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "SpriteCreator.h"
+@implementation SpriteCreator
+@end
